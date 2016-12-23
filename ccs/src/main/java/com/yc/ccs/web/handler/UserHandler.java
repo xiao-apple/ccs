8ffdfc7a65a9932aadd7ca2028a9cd86f3306445
mssora <legendarylove@qq.com>
2016-12-23 22:02:19 +0800

    修正邮件乱码错误,部分完成新密码功能

--- ccs/src/main/java/com/yc/ccs/web/handler/UserHandler.java
+++ ccs/src/main/java/com/yc/ccs/web/handler/UserHandler.java
@@ -1,6 +1,9 @@
 package com.yc.ccs.web.handler;
 
+import java.util.Random;
+
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
@@ -17,18 +20,18 @@ public class UserHandler {
 	@Autowired
 	private JavaMailSender mailSender;
 	@RequestMapping("/forget")
-	public String forget(String email,String username,HttpSession session){
+	public String forget(String email,String username,HttpServletRequest request){
 		LogManager.getLogger().debug("请求UserHandler进行forget操作...");
 		LogManager.getLogger().debug("username="+username+" email="+email);
 		try {
 			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(message);
+			MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
 			helper.setFrom("mssora@163.com");
 			helper.setTo(email);
 			helper.setSubject("找回密码");
-			String hrefString = session.getServletContext().getContext("/user/getpassword")+"?username="+username;
+			String hrefString = request.getScheme()+"://"+request.getServerName()+":"+request.getLocalPort()+request.getServletContext().getContextPath()+"/user/getpassword?username="+username;
 			System.out.println(hrefString);
-			helper.setText("<a href='"+hrefString+"'>点此重置密码</a>如果链接不可用,拷贝"+hrefString+"到地址栏", true);;
+			helper.setText("<a href='"+hrefString+"'>点此重置密码</a>如果链接不可用,拷贝"+hrefString+" 到地址栏", true);;
 			mailSender.send(message);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -36,5 +39,12 @@ public class UserHandler {
 		}
 		
 		return "redirect:/page/forgetSuccess.jsp";
+	}
+	
+	@RequestMapping("/getpassword")
+	public String getpassword(){
+		Random random = new Random();
+		String randPasswordString = random.nextInt(900000)+100000+"";
+		return "redirect:/page/getpassword";
 	}
 }
